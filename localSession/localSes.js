b25ab2758aa936a1c7527132a727dea16a4e463c
K7-Murugesan <kesavan.uniq@gmail.com>
2023-10-25 11:22:24 +0530

    added new comments

--- localSession/localSes.js
+++ localSession/localSes.js
@@ -10,9 +10,10 @@ let obj = {
 
 let newArr = ['hi','hello','world']
 
-localStorage.setItem("mySession", JSON.stringify(obj)) //here im converting normal obj to json
+localStorage.setItem("mySession", JSON.stringify(obj)) //stringify is used to convert normal obj to json
 
-let a = JSON.parse(localStorage.getItem("mySession"))  // here im retrieving data from session then im converting Json to nomral object
+let a = JSON.parse(localStorage.getItem("mySession"))  /* here im retrieving data from 
+                                                    session or local then im converting Json to nomral object */
 console.log(  a );
 
 
